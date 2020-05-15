f59afc6c0110083941551961d55449d530a812a8
valer1435 <valp998@gmail.com>
2020-05-15 15:42:45 +0300

    fixes

--- src/main/java/ru/mail/polis/pokrovskiy/STable.java
+++ src/main/java/ru/mail/polis/pokrovskiy/STable.java
@@ -11,7 +11,6 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
